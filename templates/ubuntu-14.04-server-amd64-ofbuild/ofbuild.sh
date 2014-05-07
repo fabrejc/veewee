a6448998d6de30bd2f01db488d4d7e8014290a71
Jean-Christophe Fabre <fabrejc@supagro.inra.fr>
2014-05-07 10:56:30 +0200

    Update of ofbuild templates

---
+++ templates/ubuntu-14.04-server-amd64-ofbuild/ofbuild.sh
@@ -0,0 +1,2 @@
+
+apt-get install -y g++ gfortran cmake texlive-latex-extra texlive-fonts-extra latex2html doxygen libboost-all-dev libqt4-dev libgdal1-dev libgeos++-dev git subversion p7zip-full gnuplot graphviz
